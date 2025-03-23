bc95818dab6ec75e5e7c8e7578d2c94ea4000f4e
Kodili118 <nwejedumebi0@gmail.com>
2025-03-23 23:00:19 +0100

    practice_1

---
+++ week-2/class work.py
@@ -0,0 +1,6 @@
+matric_no = "20120611062"
+score = 79
+grade = "A"
+
+formatted_string = f"{matric_no} ; {score} ; {grade}"
+print(formatted_string)
---
+++ week-2/ex.1.py
@@ -0,0 +1,7 @@
+message = "My name is"
+name = "Dumebi"
+age = "I am 18 years"
+course = "and a computer science major"
+
+formatted_string = f"{message} ; {name} ; {age} ; {course}"
+print(formatted_string)
