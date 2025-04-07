b1b2683269808cb575c8c76ac703210f0a81f610
Kodili118 <nwejedumebi0@gmail.com>
2025-04-07 20:53:25 +0100

    week-4

---
+++ week-4/classwork-1.py
@@ -0,0 +1,10 @@
+def calculate_area(radius):
+    pi = 3.14
+    area = pi * radius ** 2
+  #function to calculate the area of a circle 
+
+radius = float(input("Enter radius: "))
+print("The area of a circle is: ", calculate_area(radius))
+
+
+    
